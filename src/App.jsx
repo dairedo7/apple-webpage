ecea946eb90f29323e77dbe0ec95dd8a2dac606a
Vladyslav Svitana <vladik719@gmail.com>
2024-12-21 22:56:47 +0100

    [feat]: hero, navbar, highlights implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import './index.css';
+import React from 'react';
+import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
+import Highlights from './components/Highlights.jsx';
 
 function App() {
-  return <h1 className="text-center text-3xl">Hello there!</h1>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
+});
